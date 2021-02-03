a2beb39e564d1825388c9f6af2ee373cff689b8b
charlesmagalhaes <charlesrm29@gmail.com>
2021-02-02 23:14:16 -0300

    Projeto Criado

---
+++ src/application/program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Client clie = new Client(null, null, null);
+	}
+
+}
